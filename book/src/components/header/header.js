63e2cb9150088a4eabdb81c0aaf2857280a818c3
Sergun Pavel <axez1@mail.ru>
2025-03-04 18:25:25 +0300

    приложение по поиску книг

---
+++ book/src/components/header/header.js
@@ -0,0 +1,32 @@
+import { DivComponent } from "../../common/div-component";
+import './header.css';
+
+export class Header extends DivComponent {
+  constructor(appState) {
+    super();
+    this.appState = appState;
+  }
+
+  render() {
+    this.el.classList.add('header');
+    this.el.innerHTML = `
+      <div>
+        <img src="/static/logo.svg" alt="logotype" />
+      </div>
+      <div class="menu">
+        <a class="menu__item" href="#">
+          <img src="/static/search.svg" alt="search icon" />
+          Поиск книг
+        </a>
+        <a class="menu__item" href="#favorites">
+          <img src="/static/favorites.svg" alt="favorite icon" />
+          Избранное
+          <div class="menu__counter">
+            ${this.appState.favorites.length}
+          </div>
+        </a>
+      </div>
+    `;
+    return this.el;
+  }
+}
